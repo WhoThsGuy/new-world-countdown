e3906bdb139b35214a033b09c39ba29a2e52b515
Artem Repnikov <artemrepnikov@live.com>
2021-09-26 15:28:51 +0300

    fix for init data

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -5,31 +5,33 @@ import TimeBox from "src/TimeBox";
 
 const END_DATE = new Date("Sep 28, 2021 08:00:00 GMT+2").getTime();
 
+const getTime = () => {
+  const startDate = new Date().getTime();
+  const diff = END_DATE - startDate;
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  return {
+    days,
+    hours,
+    minutes,
+    seconds,
+  };
+};
+
 const Box = memo(({ time, children }) => (
   <TimeBox time={time}>{children}</TimeBox>
 ));
 
 const App = () => {
-  const [state, setState] = useState({
-    days: 0,
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
-  });
+  const [state, setState] = useState(getTime);
 
   useEffect(() => {
     let interval = setInterval(() => {
-      const startDate = new Date().getTime();
-      const diff = END_DATE - startDate;
-
-      const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-      const hours = Math.floor(
-        (diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      );
-      const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((diff % (1000 * 60)) / 1000);
-
-      setState({ days, hours, minutes, seconds });
+      setState(getTime);
     }, 1000);
 
     return () => {
